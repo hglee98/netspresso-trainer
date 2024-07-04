7c620d2d2a38d17d1b8ae5e2c43a138b67c72088
hglee98 <hyungeun.lee@nota.ai>
2024-07-04 08:25:36 +0000

    refactor `YOLOFastestHeadV2` to work with `YOLOFastestLoss`

--- src/netspresso_trainer/models/heads/detection/experimental/yolo_fastest_head.py
+++ src/netspresso_trainer/models/heads/detection/experimental/yolo_fastest_head.py
@@ -1,4 +1,4 @@
-from typing import List 
+from typing import List, Union, Tuple 
 from omegaconf import DictConfig
 import torch 
 import torch.nn as nn 
@@ -6,7 +6,6 @@ import math
 
 from ....op.custom import SeparableConvLayer
 from ....utils import AnchorBasedDetectionModelOutput 
-from .detection import AnchorGenerator
 
 
 class YOLOFastestHeadV2(nn.Module): 
@@ -16,29 +15,25 @@ class YOLOFastestHeadV2(nn.Module):
         intermediate_features_dim: List[int], 
         params: DictConfig) -> None:
         super().__init__()
-        num_anchors = 3 # TODO
-        anchors = params.anchors
-        num_anchors = len(anchors[0]) // 2
-        self.anchors = anchors
-        tmp_cell_anchors = []
-        for a in self.anchors: 
-            a = torch.tensor(a).view(-1, 2)
-            wa = a[:, 0:1]
-            ha = a[:, 1:]
-            base_anchors = torch.cat([-wa, -ha, wa, ha], dim=-1)/2
-            tmp_cell_anchors.append(base_anchors) 
-        self.anchor_generator = AnchorGenerator(sizes=((128),)) # TODO: dynamic image_size, and anchor_size as a parameters
-        self.anchor_generator.cell_anchors = tmp_cell_anchors
-        num_anchors = self.anchor_generator.num_anchors_per_location()[0]
+        anchors: List[Union[int, float]] = params.anchors
+        num_anchors: int = len(anchors[0]) // 2
+        anchor_tensor = torch.FloatTensor(anchors).view(len(anchors), num_anchors, 2) 
+        anchor_tensor = anchor_tensor.unsqueeze(2).unsqueeze(2) 
         in_channel = intermediate_features_dim[0]
         self.cls_head = YOLOFastestClassificationHead(in_channel, num_anchors, num_classes)  
         self.reg_head = YOLOFastestRegressionHead(in_channel, num_anchors) 
+        self.register_buffer("anchors", anchor_tensor)
 
-    def forward(self, x): 
-        anchors = torch.cat(self.anchor_generator(x), dim=0)
+    def forward(self, x: List) -> AnchorBasedDetectionModelOutput: 
         cls_logits, objs = self.cls_head(x) 
         bbox_regression = self.reg_head(x)
-        return AnchorBasedDetectionModelOutput(anchors=anchors, cls_logits=cls_logits, bbox_regression=bbox_regression)
+        preds = list() 
+        for layer_idx in range(len(x)): 
+            bbox, obj, cls_logit = bbox_regression[layer_idx], objs[layer_idx], cls_logits[layer_idx] 
+            pred = torch.cat([bbox, obj, cls_logit], dim=-1)
+            preds.append(pred)
+
+        return AnchorBasedDetectionModelOutput(pred=preds, anchors=self.anchors)
 
 def yolo_fastest_head_v2(num_classes, intermediate_features_dim, conf_model_head) -> YOLOFastestHeadV2:
     return YOLOFastestHeadV2(num_classes=num_classes, 
@@ -73,32 +68,30 @@ class YOLOFastestClassificationHead(nn.Module):
         self.num_classes = num_classes 
         self.num_anchors = num_anchors
     
-    def forward(self, x): 
+    def forward(self, x: List) -> Tuple[List[torch.Tensor], List[torch.Tensor]]: 
         all_cls_logits = [] 
         all_objs = []
         out1 = self.layer_1(x[0])
         out2 = self.layer_2(x[1])
         outputs = [out1, out2]
 
-        for idx, features in enumerate(x): 
-            cls_logits = outputs[idx]
+        for o in outputs: 
+            cls_logits = o
             objectness = cls_logits
             cls_logits = self.cls_logits(cls_logits)
             objectness = self.obj(objectness)
             
-            # Permute classification output from (N, A * K, H, W) to (N, HWA, K).
+            # Permute classification output from (N, A * K, H, W) to (N, A, H, W, K).
             N, _, H, W = cls_logits.shape
             cls_logits = cls_logits.view(N, -1, self.num_classes, H, W)
-            cls_logits = cls_logits.permute(0, 3, 4, 1, 2)
-            cls_logits = cls_logits.reshape(N, -1, self.num_classes)  # Size=(N, HWA, K)
+            cls_logits = cls_logits.permute(0, 1, 3, 4, 2)
 
             all_cls_logits.append(cls_logits)
 
-            # Permute objectness output from (N, A, H, W) to (N, HWA, 1).
+            # Permute objectness output from (N, A, H, W) to (N, A, H, W, 1).
             N, _, H, W = objectness.shape
             objectness = objectness.view(N, -1, 1, H, W)
-            objectness = objectness.permute(0, 3, 4, 1, 2)
-            objectness = objectness.reshape(N, -1, 1)  # Size=(N, HWA, 1)
+            objectness = objectness.permute(0, 1, 3, 4, 2) 
             all_objs.append(objectness)
         
         return all_cls_logits, all_objs
@@ -132,11 +125,10 @@ class YOLOFastestRegressionHead(nn.Module):
             bbox_regression = outputs[idx]
             bbox_regression = self.bbox_reg(bbox_regression)
 
-            # Permute bbox regression output from (N, 4 * A, H, W) to (N, HWA, 4).
+            # Permute bbox regression output from (N, 4 * A, H, W) to (N, A, H, W, 4).
             N, _, H, W = bbox_regression.shape
             bbox_regression = bbox_regression.view(N, -1, 4, H, W)
-            bbox_regression = bbox_regression.permute(0, 3, 4, 1, 2)
-            bbox_regression = bbox_regression.reshape(N, -1, 4)  # Size=(N, HWA, 4)
+            bbox_regression = bbox_regression.permute(0, 1, 3, 4, 2)
 
             all_bbox_regression.append(bbox_regression)
         

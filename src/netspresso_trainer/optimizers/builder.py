460372628e9e49288c874c6b904660778780bdeb
hglee98 <hyungeun.lee@nota.ai>
2024-08-27 06:47:13 +0000

    Add optimizer params

--- src/netspresso_trainer/optimizers/builder.py
+++ src/netspresso_trainer/optimizers/builder.py
@@ -17,10 +17,61 @@
 from typing import Literal
 
 import torch.nn as nn
+import re
 from omegaconf import DictConfig
 
 from .registry import OPTIMIZER_DICT
 
+def get_layerwise_params(model, optimizer_conf):
+    default_lr = optimizer_conf.lr
+    default_weight_decay = optimizer_conf.weight_decay
+
+    param_groups = [
+        {
+            'params': [],
+            'lr': default_lr / 10,
+            'weight_decay': default_weight_decay,
+            'name': 'backbone_non_norm'
+        },
+        {
+            'params': [],
+            'lr': default_lr / 10,
+            'weight_decay': 0.,
+            'name': 'backbone_norm'
+        },
+        {
+            'params': [],
+            'lr': default_lr,
+            'weight_decay': 0.,
+            'name': 'neck_head_norm'
+        },
+        {
+            'params': [],
+            'lr': default_lr,
+            'weight_decay': default_weight_decay,
+            'name': 'other'
+        }
+    ]
+
+    # reg patterns
+    patterns = [
+        (r'^(?=.*backbone)(?!.*(?:norm|bn)).*$', 0),
+        (r'^(?=.*backbone)(?=.*(?:norm|bn)).*$', 1),
+        (r'^(?=.*(?:neck|head))(?=.*(?:norm|bn|bias)).*$', 2)
+    ]
+
+    for name, param in model.named_parameters():
+        assigned = False
+        for pattern, group_idx in patterns:
+            if re.match(pattern, name):
+                param_groups[group_idx]['params'].append(param)
+                assigned = True
+                break
+        if not assigned:
+            param_groups[3]['params'].append(param)
+
+    param_groups = [g for g in param_groups if g['params']]
+    return param_groups
 
 def build_optimizer(
     model_or_params,
@@ -30,6 +81,15 @@ def build_optimizer(
 
     opt_name: Literal['sgd', 'adam', 'adamw', 'adamax', 'adadelta', 'adagrad', 'rmsprop'] = optimizer_conf.name.lower()
     assert opt_name in OPTIMIZER_DICT
-
-    optimizer = OPTIMIZER_DICT[opt_name](parameters, optimizer_conf)
+    if getattr(optimizer_conf, 'params') is not None:
+        """
+        E.g.:
+            ^(?=.*a)(?=.*b).*$  means including a and b
+            ^(?=.*(?:a|b)).*$   means including a or b
+            ^(?=.*a)(?!.*b).*$  means including a, but not b
+        """
+        params_groups = get_layerwise_params(model_or_params, optimizer_conf)
+        optimizer = OPTIMIZER_DICT[opt_name](params_groups, optimizer_conf)
+    else:
+        optimizer = OPTIMIZER_DICT[opt_name](parameters, optimizer_conf)
     return optimizer

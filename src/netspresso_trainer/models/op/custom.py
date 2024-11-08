c23a7c47571223cb39f0cbb531ee1e69177bb46b
hglee98 <hyungeun.lee@nota.ai>
2024-11-08 04:27:42 +0000

    [feat] Init AConv

--- src/netspresso_trainer/models/op/custom.py
+++ src/netspresso_trainer/models/op/custom.py
@@ -877,3 +877,8 @@ class ELAN(nn.Module):
         x4 = self.conv3(x3)
         x5 = self.conv4(torch.cat([x1, x2, x3, x4], dim=1))
         return x5
+
+
+class AConv(nn.Module):
+    def __init__(self):
+        super().__init__()

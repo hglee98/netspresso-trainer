f4759f7fbfe0c799d362194740cf47577d3d2a90
hglee98 <hyungeun.lee@nota.ai>
2024-11-11 06:46:39 +0000

    [feat] Implement yolov9 neck

--- src/netspresso_trainer/models/registry.py
+++ src/netspresso_trainer/models/registry.py
@@ -38,7 +38,7 @@ from .heads.classification import fc, fc_conv
 from .heads.detection import anchor_decoupled_head, anchor_free_decoupled_head, rtdetr_head, yolo_fastest_head_v2
 from .heads.pose_estimation import rtmcc
 from .heads.segmentation import all_mlp_decoder
-from .necks import fpn, lightfpn, rtdetr_hybrid_encoder, yolopafpn
+from .necks import fpn, lightfpn, rtdetr_hybrid_encoder, yolopafpn, yolov9fpn
 
 MODEL_BACKBONE_DICT: Dict[str, Callable[..., nn.Module]] = {
     'resnet': resnet,
@@ -58,6 +58,7 @@ MODEL_NECK_DICT: Dict[str, Callable[..., nn.Module]] = {
     'fpn': fpn,
     'lightfpn': lightfpn,
     'yolopafpn': yolopafpn,
+    'yolov9fpn': yolov9fpn,
     'rtdetr_hybrid_encoder': rtdetr_hybrid_encoder,
 }
 
